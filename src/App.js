dcd3072498c1ba8016c1945c924c3db7acc1cb07
AkshayKate10 <akshaykate444@gmail.com>
2024-11-28 23:31:21 +0530

    changing title of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import { reduxstore } from "./Store/Store";
 import Main from "./Components/Main";
 
 function App() {
+  useEffect(() => {
+    document.title = "akshay";
+  }, []);
   return (
     <Provider store={reduxstore}>
       <div className="App">
